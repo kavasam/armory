ef175df3bc33e7154967f5f3003b0e63af2a81f7
realaravinth <realaravinth@batsense.net>
2021-12-19 15:26:09 +0000

    Deploying to gh-pages from  @ 2c62717838d01cc0b3d93c4a877bfbc7236efe87 🚀

---
+++ crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["armory","tests_migrate"];
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["armory"] = {"name":"","files":["data.rs","main.rs","settings.rs"]};
+sourcesIndex["tests_migrate"] = {"name":"","files":["settings.rs","tests-migrate.rs"]};
+createSourceSidebar();
